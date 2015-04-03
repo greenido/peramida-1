0d0753bb97983d8cc32289f9e497e1a4d039e5a1
Ido Green <greenido@gmail.com>
2015-04-03 15:26:58 +0300

    Alert the user on success

--- js/main.js
+++ js/main.js
@@ -10,14 +10,55 @@ $( document ).ready(function() {
     var tmpNo = Math.floor((Math.random() * 10) + 1);
     $("#l1-"+i).val(tmpNo);
   };
-  
+
+  // lets have only numners as answers
+  $("input").keydown(function (e) {
+    // Allow: backspace, delete, tab, escape, enter and .
+    if ($.inArray(e.keyCode, [46, 8, 9, 27, 13, 110, 190]) !== -1 ||
+         // Allow: Ctrl+A
+        (e.keyCode == 65 && e.ctrlKey === true) || 
+         // Allow: home, end, left, right, down, up
+        (e.keyCode >= 35 && e.keyCode <= 40)) {
+             // let it happen, don't do anything
+             return;
+    }
+    // Ensure that it is a number and stop the keypress
+    if ((e.shiftKey || (e.keyCode < 48 || e.keyCode > 57)) && (e.keyCode < 96 || e.keyCode > 105)) {
+        e.preventDefault();
+    }
+  });
+
+
+
   // calculate if it's the correct answer
   $("input").on("blur", function(){
     //
     var curId = $(this).attr('id');
-    var curVal = $(this).val();
-    console.log("Got a change! on id: " + curId + " val:" + curVal);
-    
+    var curVal = Number($(this).val());
+    console.log("A new answer on id: " + curId + " val:" + curVal);
+    var curLevel = Number(curId.substring(1,2));
+    var curCell  = Number(curId.substring(3));  
+    console.log("level: " + curLevel + " cell:" + curCell);
+
+    var isCorrect = false;
+    var cellLeftId  = "#l" + (curLevel-1) + "-" + curCell;
+    var cellRightId = "#l" + (curLevel-1) + "-" + (1 + curCell );
+    var cellLeft = Number($(cellLeftId).val());
+    var cellRight = Number($(cellRightId).val());
+    if (curVal === cellLeft + cellRight ) {
+      //
+      $("#malert").show();
+      setTimeout(function() { 
+        $("#malert").hide();
+      }, 2000);
+
+      $(this).css("background-color", "white");
+    }
+    else {
+       $(this).css("background-color", "red");
+    }
   });
 
+
+  
 });
